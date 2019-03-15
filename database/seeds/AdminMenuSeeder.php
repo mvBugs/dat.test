87de52ed80b8bb65bbebee90ed905731c216eb95
vagrant <vagrant@homestead>
2019-03-15 11:05:02 +0000

    [*]

--- database/seeds/AdminMenuSeeder.php
+++ database/seeds/AdminMenuSeeder.php
@@ -68,62 +68,6 @@ class AdminMenuSeeder extends MenusBaseSeeder
                                     'pattern_url' => '\S*type=pages\S*',
                                 ]
                             ],
-                            [
-                                'name' => 'Новости',
-                                'path' => url('admin/nodes?type=news'),
-                                'data' => [
-                                    'permissions' => [],
-                                    'icon' => '',
-                                    'pattern_url' => '\S*type=news\S*',
-                                ]
-                            ],
-                        ],
-                    ],
-
-                    [
-                        'name' => 'Списки',
-                        'path' => '',
-                        'data' => [
-                            'permissions' => [],
-                            'icon' => 'fa fa-list',
-                        ],
-                        'children' => [
-                            [
-                                'name' => 'Casinos',
-                                'path' => url('admin/items?type=casinos'),
-                                'data' => [
-                                    'permissions' => [],
-                                    'icon' => '',
-                                    'pattern_url' => '\S*type=casinos\S*',
-                                ]
-                            ],
-                            [
-                                'name' => 'Last streams',
-                                'path' => url('admin/items?type=last_streams'),
-                                'data' => [
-                                    'permissions' => [],
-                                    'icon' => '',
-                                    'pattern_url' => '\S*type=last_streams\S*',
-                                ]
-                            ],
-                            [
-                                'name' => 'Sports Betting',
-                                'path' => url('admin/items?type=sports_betting'),
-                                'data' => [
-                                    'permissions' => [],
-                                    'icon' => '',
-                                    'pattern_url' => '\S*type=sports_betting\S*',
-                                ]
-                            ],
-                            [
-                                'name' => 'Popular questions',
-                                'path' => url('admin/items?type=popular_questions'),
-                                'data' => [
-                                    'permissions' => [],
-                                    'icon' => '',
-                                    'pattern_url' => '\S*type=popular_questions\S*',
-                                ]
-                            ],
                         ],
                     ],
 
@@ -136,12 +80,12 @@ class AdminMenuSeeder extends MenusBaseSeeder
                         ],
                         'children' => [
                             [
-                                'name' => 'Individual bonus',
-                                'path' => url('/admin/forms?type=individual_bonus'),
+                                'name' => 'Напишите нам',
+                                'path' => url('/admin/forms?type=write'),
                                 'data' => [
                                     'permissions' => ['form.read',],
                                     'icon' => '',
-                                    'pattern_url' => '\S*type=individual_bonus\S*',
+                                    'pattern_url' => '\S*type=write\S*',
                                 ],
                             ],
                         ],
@@ -164,7 +108,7 @@ class AdminMenuSeeder extends MenusBaseSeeder
                             'icon' => 'fa fa-th',
                         ],
                         'children' => [
-                            [
+                            /*[
                                 'name' => 'Рубрикатор',
                                 'path' => '',
                                 'data' => [
@@ -191,7 +135,7 @@ class AdminMenuSeeder extends MenusBaseSeeder
                                         ]
                                     ],
                                 ],
-                            ],
+                            ],*/
                             [
                                 'name' => 'Меню',
                                 'path' => url('admin/menu'),
@@ -230,14 +174,14 @@ class AdminMenuSeeder extends MenusBaseSeeder
                         ],
                     ],
 
-                    [
+                    /*[
                         'name' => 'Пользователи',
                         'path' => url('admin/users'),
                         'data' => [
                             'permissions' => ['user.read'],
                             'icon' => 'fa fa-users',
                         ],
-                    ],
+                    ],*/
 
                 ],
             ],
