723522f2c739debe4255d6a954c68b5ced9fa207
vagrant <vagrant@homestead>
2019-03-25 14:38:13 +0000

    [*]

--- resources/views/fronts/pages/metallic.blade.php
+++ resources/views/fronts/pages/metallic.blade.php
@@ -394,4 +394,172 @@
         })
     </script>
     @include('fronts.layouts.inc.footer-inner')
+    <div class="modal fade" id="table-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <div class="modal-body">
+                    <div class="norms">
+                        {!! \BlockVisualEditor::render('modal_wood', '
+                            <div class="norms__wrapper">
+                            <div class="new-image">
+                                <img src="/its-client/img/metallic-image.png" alt="">
+                                <div class="wood-content__right-text">
+                                 <span>
+                                     "КБ - № барабана
+                                 </span>
+                                    <span>Ø1 (D1) - диаметры щек</span>
+                                    <span>Ø2 (D2) - диаметр шейки</span>
+                                    <span>Ø3 (D3) - наружные диаметры втулок</span>
+                                    <span>Ø4 (D4) - внутренние диаметры втулок</span>
+                                    <span>Ø5 (D5) - диаметры поводковых отверстий</span>
+                                    <span>е - расстояния от центров щек до центров поводковых отверстий щеки к шейке</span>
+                                    <span>L1 - габаритная ширина барабана</span>
+                                    <span>L2 - ширина шейки</span>
+                                    <span>K1 - ширина отверстия для ввода кабеля</span>
+                                    <span>K2 - длина отверстия для ввода кабеля</span>
+                                </div>
+                            </div>
+                            <div class="typography">
+                                        <table>
+                                        <caption>Нормы загрузки</caption>
+                                        <tr>
+                                            <th rowspan="3">Номер <br> барабана</th>
+                                            <th rowspan="3">Масса, <br> кг </th>
+                                            <th rowspan="3"> Кол-во комплектов* в <br> фуре (горизонтально)</th>
+                                        </tr>
+                                        <tr></tr>
+                                        <tr></tr>
+                                            <tr>
+                                                <td>8</td>
+                                                <td>34</td>
+                                                <td>500</td>
+                                            </tr>
+                                            <tr>
+                                                <td>8 а</td>
+                                                <td>36.5</td>
+                                                <td>500</td>
+                                            </tr>
+                                            <tr>
+                                                <td>8 б</td>
+                                                <td>38</td>
+                                                <td>500</td>
+                                            </tr>
+                                            <tr>
+                                                <td>10</td>
+                                                <td>39</td>
+                                                <td>200</td>
+                                            </tr>
+                                            <tr>
+                                                <td>10 а</td>
+                                                <td>55</td>
+                                                <td>200</td>
+                                            </tr>
+                                            <tr>
+                                                <td>12</td>
+                                                <td>99</td>
+                                                <td>200</td>
+                                            </tr>
+                                            <tr>
+                                                <td>12 а</td>
+                                                <td>107</td>
+                                                <td>200</td>
+                                            </tr>
+                                            <tr>
+                                                <td>14</td>
+                                                <td>165</td>
+                                                <td>120</td>
+                                            </tr>
+                                            <tr>
+                                                <td>14 а</td>
+                                                <td>152</td>
+                                                <td>120</td>
+                                            </tr>
+                                            <tr>
+                                                <td>14 б</td>
+                                                <td>171</td>
+                                                <td>120</td>
+                                            </tr>
+                                            <tr>
+                                                <td>14 г</td>
+                                                <td>176</td>
+                                                <td>120</td>
+                                            </tr>
+                                            <tr>
+                                                <td>16</td>
+                                                <td>241</td>
+                                                <td>100</td>
+                                            </tr>
+                                            <tr>
+                                                <td>16 а</td>
+                                                <td>237</td>
+                                                <td>100</td>
+                                            </tr>
+                                            <tr>
+                                                <td>17</td>
+                                                <td>277</td>
+                                                <td>70</td>
+                                            </tr>
+                                            <tr>
+                                                <td>17 а</td>
+                                                <td>295</td>
+                                                <td>70</td>
+                                            </tr>
+                                            <tr>
+                                                <td>18</td>
+                                                <td>422</td>
+                                                <td>50</td>
+                                            </tr>
+                                            <tr>
+                                                <td>18 а</td>
+                                                <td>422</td>
+                                                <td>50</td>
+                                            </tr>
+                                            <tr>
+                                                <td>20</td>
+                                                <td>584</td>
+                                                <td>40</td>
+                                            </tr>
+                                            <tr>
+                                                <td>20 а</td>
+                                                <td>555</td>
+                                                <td>40</td>
+                                            </tr>
+                                            <tr>
+                                                <td>20 б</td>
+                                                <td>631</td>
+                                                <td>40</td>
+                                            </tr>
+                                            <tr>
+                                                <td>22</td>
+                                                <td>759</td>
+                                                <td>30</td>
+                                            </tr>
+                                        </table>
+                                        <a href="#">
+                                            <svg class="icon-svg icon-svg-download "><use xlink:href="/its-client/img/sprite.svg#download"></use></svg>
+                                            <span></span>
+                                            <span>Нормы загрузки</span>
+                                        </a>
+                                        <a href="#">
+                                            <svg class="icon-svg icon-svg-download "><use xlink:href="/its-client/img/sprite.svg#download"></use></svg>
+                                            <span></span>
+                                            <span>Нормы намотки кабеля</span>
+                                        </a>
+                                        <h5 style="color: white">
+                                            * - Данные в таблице приведены справочно и могут варьироваться в зависимости от размера полуприцепов. В силу особенностей погрузки может быть перегруз или недогруз до 10% .
+                                        </h5>
+                                        <p>
+                                            <img src="/its-client/img/norms-img.png" alt="">
+
+                                        </p>
+                            </div>
+                        </div>
+                        ') !!}
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
