02bb6b442f0a5bdb64bdaf683f214101cbeb7c54
Микола Войтович <m.voitovych@ideil.com>
2020-05-01 08:34:52 +0300

    update meta

--- resources/views/fronts/layouts/inc/begin.blade.php
+++ resources/views/fronts/layouts/inc/begin.blade.php
@@ -1,17 +1,47 @@
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
-    <meta charset="UTF-8">
-    <meta name="title" content="{{ variable('title') }}" />
-    <meta name="keywords" content="{{ variable('keywords') }}" />
-    <meta name="description" content="{{ variable('description') }}" />
-    <meta name="robots" content="nosnippet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Cache-Control" content="no-cache">
-    <meta name="google-site-verification" content="G5kWTYcd9OW1TJEF8Tjrt6bqUl0EulTQ89zRM8Hpgl4" />
-    <title>{{ variable('app_name') }}</title>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, viewport-fit=cover"/>
+    <meta name="HandheldFriendly" content="True"/>
+    <meta http-equiv="cleartype" content="on"/>
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="format-detection" content="address=no"/>
+
+    <title>
+        {{ variable('app_name') }}
+    </title>
+    <meta name="keywords" content="{{ variable('keywords') }}"/>
+    <meta name="description" content="{{ variable('description') }}"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content=""/>
+    <meta name="copyright" content="(c)">
+    <meta http-equiv="Reply-to" content="noreply@datsuk-truns.com">
+
+    <meta name="theme-color" content="#000000">
+
+
+    <!-- Twitter card -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@twitter" />
+    <meta name="twitter:creator" content="@twitter" />
+    <meta name="twitter:image" content="https://example.com/example.png" />
+    <meta name="twitter:description" content="{{ variable('description') }}" />
+    <meta name="twitter:title" content="{{ variable('title') }}" />
+
+
+    <!-- Open Graph -->
+    <meta property="og:title" content="{{ variable('title') }}" />
+    <meta property="og:description" content="{{ variable('description') }}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ url()->current() }}" />
+    <meta property="og:image" content="https://example.com/example1200x630.png" />
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:site_name" content="{{ variable('title') }}" />
+
     <!-- Vendor styles plugins -->
     <!-- build:css css/plugins.css -->
     <link rel="stylesheet" href="/its-client/vendor/reset-css/reset.css">
